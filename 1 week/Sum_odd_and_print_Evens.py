f8e6ade4a18b08c59b04e56902ac630074eb1b96
ashleshaa12 <ashlesha123c@gmail.com>
2025-08-02 21:56:36 +1200

    Update Sum_odd_and_print_Evens.py

--- 1 week/Sum_odd_and_print_Evens.py
+++ 1 week/Sum_odd_and_print_Evens.py
@@ -8,19 +8,22 @@ def Sum_odd_and_print_Evens():
     odd_sum = 0
     print_evens = []
     even_sum = 0
+    total_sum = 0
     i = 1
 
-    while i < num:
+    while i <= num:
+        total_sum += i
         if i % 2 == 1:
             odd_sum += i
-    else:
-        even_numbers.append(i)
-        even_sum += i
-    i += 1
+        else:
+            print_evens.append(i)
+            even_sum += i
+        i += 1
 
-    print("Even numbers between 1 and {num} are:", even_numbers)
+    print("Even numbers between 1 and {num} are:", print_evens)
     print("sum of odd numbers:", odd_sum)
     print("sum of even numbers:", even_sum)
+    print("sum of th total numbrs between 1 and given number is:", total_sum)
 
 if __name__ == "__main__":
     Sum_odd_and_print_Evens()
