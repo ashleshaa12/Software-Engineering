6cd9c6b13317bd016335076534b10998f89e19a4
ashleshaa12 <ashlesha123c@gmail.com>
2025-08-08 21:06:19 +1200

    Update temp.py

--- week1/temp.py
+++ week1/temp.py
@@ -1,12 +1,3 @@
-# Week1- Activity 3: Temperature Analysis Using Python and NumPy
-# Develop a Python project to perform the following tasks:
-# Calculate and print the average temperature for the week using NumPy.
-# Temperature data (in °C): []
-# Determine and display the highest and lowest recorded temperatures.
-# Convert all temperatures to Fahrenheit and print the converted values.
-# (Use the formula: F = C × 9/5 + 32)
-# Identify and print the indices of days where the temperature exceeded 20°C.
- 
 
 
 import numpy as np
