c751eb845237645d1d0dac98d22343e3344b8880
ashleshaa12 <ashlesha123c@gmail.com>
2025-08-08 20:58:59 +1200

    Assignment

---
+++ week1/temp.py
@@ -0,0 +1,35 @@
+# Week1- Activity 3: Temperature Analysis Using Python and NumPy
+# Develop a Python project to perform the following tasks:
+# Calculate and print the average temperature for the week using NumPy.
+# Temperature data (in °C): []
+# Determine and display the highest and lowest recorded temperatures.
+# Convert all temperatures to Fahrenheit and print the converted values.
+# (Use the formula: F = C × 9/5 + 32)
+# Identify and print the indices of days where the temperature exceeded 20°C.
+ 
+
+
+import numpy as np
+
+temp = np.array([0.0, 5.2, 3.1, 0.0, 12.4, 0.0, 7.5])
+
+total_rainfall = np.sum(temp)
+round_rainfall = round(total_rainfall, 2)
+
+Average_rainfall = np.mean(temp)
+
+round_avg = round(Average_rainfall, 2)
+
+No_rain = np.where( temp <= 0)
+
+Index = np.where(temp > 5)
+
+#round_index = round(Index, 2)
+
+print("The total rainfall for the week:", round_rainfall)
+
+print("The Average rainfall in a week is:", round_avg)
+
+print("the week days where no rainfall was seen(0=monday, 6=sunday):", No_rain[0])
+
+print("the week days where no rainfall was seen(0=monday, 6=sunday):", Index[0])
